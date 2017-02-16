ffaca180e86ee5598b09091e3ada59be11263e03
byuranan <yuranan.k@icloud.com>
2017-02-16 15:26:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,8 @@
 <?php
 $access_token = 'Gqae7mFgYuFurZ72d1wPGGjtZi7W72kkHYdplUFy3U+U4n+CEO5gKUQmnOT5EiGufBy8473V/QfSsw7z7c3qxSwp5RaJZwPcQmRdzMTtxnPcPdqRbozovd0+jbY9p/qShPFHOLxnVLEChriBWncGugdB04t89/1O/w1cDnyilFU=';
 //Test
-$text1 = if ($text == "hello");
-		echo "Done";
+$text1 = if ($text = "hello");
+		echo "Done".;
 //Test
 // Get POST body content
 $content = file_get_contents('php://input');
