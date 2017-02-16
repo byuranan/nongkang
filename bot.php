b3c7f01b0b0251605a416938a6395edfebf05778
byuranan <yuranan.k@icloud.com>
2017-02-16 20:26:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text')
-		if ($event[0] == "hello") {
+		if ($event['message']['text'] == array["hello"]) {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
