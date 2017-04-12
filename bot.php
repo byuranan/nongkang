f34c081cb1eef77687e3953f9be40595de654c1d
byuranan <yuranan.k@icloud.com>
2017-04-12 12:12:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,37 +4,38 @@ $access_token = 'Gqae7mFgYuFurZ72d1wPGGjtZi7W72kkHYdplUFy3U+U4n+CEO5gKUQmnOT5EiG
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$url = 'https://api.line.me/v2/bot/message/reply';
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 // Validate parsed JSON data
 	if($events['events'][0]['message']['text'] == "สวัสดี"){
  		 $post = array();
  		 $post['replyToken'] = $events['events'][0]['replyToken'];
  		 $post['messages'][0]['type'] = "text";
- 		 $post['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$events['events'][0]['source']['userId'];
-	}else if($events['events'][0]['message']['text'] == "ชื่ออะไร"){
+ 		 $post['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$events['events'][0]['source']['userId'];}
+	else if($events['events'][0]['message']['text'] == "ชื่ออะไร"){
   		 $post = array();
 	  	 $post['replyToken'] = $events['events'][0]['replyToken'];
   		 $post['messages'][0]['type'] = "text";
-  		 $post['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-	}else if($events['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  		 $post['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";}
+	else if($events['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
   		 $post = array();
  		 $post['replyToken'] = $events['events'][0]['replyToken'];
  		 $post['messages'][0]['type'] = "text";
- 		 $post['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-	}else{
+ 		 $post['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";}
+	else{
  		 $post = array();
  		 $post['replyToken'] = $events['events'][0]['replyToken'];
  		 $post['messages'][0]['type'] = "text";
- 		 $post['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-	}
+ 		 $post['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";}
+	
 			// Make a POST Request to Messaging API to reply to sender
 			
 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_POST, true);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
