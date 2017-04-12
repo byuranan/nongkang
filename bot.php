b12d34109d2f3158706e329c4ba39cd4eeaf7979
byuranan <yuranan.k@icloud.com>
2017-04-12 11:59:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,40 +5,36 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if($events['events'][0]['message']['text'] == "สวัสดี"){
-  $post = array();
-  $post['replyToken'] = $events['events'][0]['replyToken'];
-  $post['messages'][0]['type'] = "text";
-  $post['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$events['events'][0]['source']['userId'];
-}else if($events['events'][0]['message']['text'] == "ชื่ออะไร"){
-  $post = array();
-  $post['replyToken'] = $events['events'][0]['replyToken'];
-  $post['messages'][0]['type'] = "text";
-  $post['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-}else if($events['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
-  $post = array();
-  $post['replyToken'] = $events['events'][0]['replyToken'];
-  $post['messages'][0]['type'] = "text";
-  $post['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else{
-  $post = array();
-  $post['replyToken'] = $events['events'][0]['replyToken'];
-  $post['messages'][0]['type'] = "text";
-  $post['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-}
+	if($events['events'][0]['message']['text'] == "สวัสดี"){
+ 		 $post = array();
+ 		 $post['replyToken'] = $events['events'][0]['replyToken'];
+ 		 $post['messages'][0]['type'] = "text";
+ 		 $post['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$events['events'][0]['source']['userId'];
+	}else if($events['events'][0]['message']['text'] == "ชื่ออะไร"){
+  		 $post = array();
+	  	 $post['replyToken'] = $events['events'][0]['replyToken'];
+  		 $post['messages'][0]['type'] = "text";
+  		 $post['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+	}else if($events['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  		 $post = array();
+ 		 $post['replyToken'] = $events['events'][0]['replyToken'];
+ 		 $post['messages'][0]['type'] = "text";
+ 		 $post['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+	}else{
+ 		 $post = array();
+ 		 $post['replyToken'] = $events['events'][0]['replyToken'];
+ 		 $post['messages'][0]['type'] = "text";
+ 		 $post['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+	}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
+			json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
@@ -46,8 +42,5 @@ if($events['events'][0]['message']['text'] == "สวัสดี"){
 
 			echo $result . "\r\n";
 				
-			
-		
-	
-
 echo "OK";
+?>
