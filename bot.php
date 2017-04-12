8f57f30650a7ce31696754cabc545c9008dbf230
byuranan <yuranan.k@icloud.com>
2017-04-12 11:08:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,6 +31,17 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => "สัส"
 			];
+		else ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
