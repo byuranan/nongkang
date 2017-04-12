e88e54938798a78a01395c466d8677bb2c49a2c9
byuranan <yuranan.k@icloud.com>
2017-04-12 14:13:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,11 +8,11 @@ $url = 'https://api.line.me/v2/bot/message/reply';
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 // Validate parsed JSON data
-	if(strstr($events['events'][0]['message']['text'],"กาก")){
+	if(strstr($events['events'][0]['message']['text'],"ทนาย")){
  		 $post = array();
  		 $post['replyToken'] = $events['events'][0]['replyToken'];
  		 $post['messages'][0]['type'] = "text";
- 		 $post['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$events['events'][0]['source']['userId'];}
+ 		 $post['messages'][0]['text'] = "สวัสดี","ว่าไง";}
 	else if($events['events'][0]['message']['text'] == "ชื่ออะไร"){
   		 $post = array();
 	  	 $post['replyToken'] = $events['events'][0]['replyToken'];
