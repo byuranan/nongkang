f56925d4a60f77cc36692d5f66762c51dcf8d0a8
byuranan <yuranan.k@icloud.com>
2017-04-17 10:00:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ $randg = array_rand($greeting, 3);
  		 $post = array();
  		 $post['replyToken'] = $events['events'][0]['replyToken'];
  		 $post['messages'][0]['type'] = "text";
- 		 $post['messages'][0]['text'] = $greeting[$randg[0]];}
+ 		 $post['messages'][0]['text'] = $greeting[$randg];}
 	else if($events['events'][0]['message']['text'] == "ชื่ออะไร"){
   		 $post = array();
 	  	 $post['replyToken'] = $events['events'][0]['replyToken'];
